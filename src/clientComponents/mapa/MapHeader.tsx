3404c7cf4b9be012028eb371dc9daf3c3576bdba
yazimel-niclis-itti <yazimel.niclis@itti.digital>
2024-06-21 16:28:19 -0300

    aproximacion

---
+++ src/clientComponents/mapa/MapHeader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MapHeader() {
+  return (
+    <header className="bg-[#1B2128] w-full h-12">
+      <p className="text-base text-white ml-8 pl-9">La Vela Puerca</p>
+      <p className="text-xs text-white ml-8 pl-9">
+        29 de Septiembre - 20:00 - Anfiteatro Jose A. Flores - San Bernardino
+      </p>
+    </header>
+  );
+}
+
+export default MapHeader;
