8aa54f3b917e2dd2ba8aee532e1c45354ab6e177
dianaatobe <diana.atobe@itti.digital>
2024-06-24 10:07:24 -0400

    wip: add seat selection/hover maplibre raw

--- src/app/components/maplibre/Map.tsx
+++ src/app/components/maplibre/Map.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useRef, useEffect, useState } from "react";
-import maplibregl from "maplibre-gl";
+import maplibregl, { MapLayerMouseEvent, MapMouseEvent } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./map.css";
 
@@ -21,6 +21,7 @@ interface Seat {
     coordinates: [number, number];
   };
 }
+
 /* 
 const centralPoint = { lat: -25.2921546, lng: -57.6573 };
 const mapBounds = new LngLatBounds(
@@ -32,8 +33,14 @@ export default function Map() {
   const [allData, setAllData] = React.useState<any>(null);
   const [seatData, setSeatData] = React.useState<Seat[]>([]);
 
+  const [hoveredSection, setHoveredSection] = React.useState(null);
+  const [selectedSection, setSelectedSection] = useState(null);
+  const [selectedSeats, setSelectedSeats] = useState<Seat[]>([]);
+  const [hoveredSeat, setHoveredSeat] = useState(null);
+
   const mapContainer = useRef(null);
   const map = useRef(null);
+
   const [lng] = useState(139.753);
   const [lat] = useState(35.6844);
   const [zoom] = useState(14);
@@ -89,13 +96,71 @@ export default function Map() {
           type: "circle",
           source: "seats",
           paint: {
-            "circle-radius": 5,
-            "circle-color": "#000",
+            "circle-radius": 8,
+            "circle-color": "#C2C3C7", // Default color
           },
         });
+        map.current.on("mousemove", "estadio", (event: MapLayerMouseEvent) => {
+          const { features } = event;
+          const hoveredSectionId = features && features[0]?.properties?.id;
+          if (hoveredSectionId == hoveredSection) {
+            return;
+          }
+          if (selectedSection && hoveredSectionId === selectedSection) {
+            setHoveredSection(null);
+          } else {
+            setHoveredSection(hoveredSectionId || null);
+          }
+        });
+
+        map.current.on("mouseover", "seats", (event: MapLayerMouseEvent) => {
+          const { features } = event;
+          console.log(features, "seat hover features event obj");
+
+          const hoveredSeatId = features && features[0]?.properties?.id;
+          setHoveredSeat(hoveredSeatId || null);
+        });
+
+        map.current.on("click", "seats", (event: MapLayerMouseEvent) => {
+          const { features } = event;
+          console.log(features, "seat click features obj");
+
+          const seatId = features && features[0]?.properties.id;
+
+          setSelectedSeats((prevSelectedSeats) => {
+            if (prevSelectedSeats.includes(seatId)) {
+              return prevSelectedSeats.filter((id) => id !== seatId);
+            } else {
+              return [...prevSelectedSeats, seatId];
+            }
+          });
+        });
+        /////////////
       });
     }
   }, [allData]);
+
+  useEffect(() => {
+    if (map.current && map.current.isStyleLoaded()) {
+      map.current.setPaintProperty("seats", "circle-color", [
+        "case",
+        ["==", ["get", "id"], hoveredSeat],
+        "#3288bd", // hover color
+        ["in", ["get", "id"], ["literal", selectedSeats]],
+        "#FF0000", // selected color
+        "#C2C3C7", // default color
+      ]);
+
+      /*  map.current.setPaintProperty("estadio", "fill-color", [
+        "case",
+        ["==", ["get", "id"], hoveredSection],
+        "#3288bd", // hover color
+        ["==", ["get", "id"], selectedSection],
+        "#FF0000", // selected color
+        "#088", // default color
+      ]); */
+    }
+  }, [hoveredSeat, selectedSeats]);
 
   return (
     <div className="map-wrap">
