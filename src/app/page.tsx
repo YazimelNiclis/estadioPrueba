520076c81f6874c1c92daaa286950ebba9b1da94
juan-mendoza-itti <juan.mendoza@itti.digital>
2024-07-08 17:54:10 -0400

    test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <>
-      <main className="flex w-full h-screen justify-center items-center">
-        <Link href="/estadio">
-          <h1 className="underline hover:text-gray-400">Go to estadio</h1>
-        </Link>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className="flex h-screen w-full items-center justify-center">
+                <Link href="/estadio">
+                    <h1 className="underline hover:text-gray-400">
+                        Go to estadio
+                    </h1>
+                </Link>
+            </main>
+        </>
+    );
 }
