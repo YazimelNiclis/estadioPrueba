88bed33eaa343ae11a9ac6aa7ea52fe46c6f2f0a
juan-mendoza-itti <juan.mendoza@itti.digital>
2024-07-08 17:53:15 -0400

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import "maplibre-gl/dist/maplibre-gl.css";
-import { NextUIProvider } from "@nextui-org/react";
-import Footer from "@/components/footer/Footer";
+import type { Metadata } from 'next';
+import './globals.css';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { NextUIProvider } from '@nextui-org/react';
+import Footer from '@/components/footer/Footer';
 
 export const metadata: Metadata = {
-  title: "estadio",
-  description: "prueba de soluciones",
+    title: 'estadio',
+    description: 'prueba de soluciones'
 };
 
 export default function RootLayout({
-  children,
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <NextUIProvider>
-          {children}
-          <Footer />
-        </NextUIProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <NextUIProvider>
+                    {children}
+                    <Footer />
+                </NextUIProvider>
+            </body>
+        </html>
+    );
 }
